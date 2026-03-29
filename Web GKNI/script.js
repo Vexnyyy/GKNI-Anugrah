da16232e0b9cee69cac61b090bdaaac01a6c3ef3
Vexnyyy <vincent.edbert2322@gmail.com>
2026-03-29 22:40:06 +0700

    Add files via upload

---
+++ Web GKNI/script.js
@@ -0,0 +1,14 @@
+console.log("JS connect");
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.getElementById("slider");
+  const next = document.querySelector(".next");
+  const prev = document.querySelector(".prev");
+
+  next.addEventListener("click", () => {
+    slider.scrollBy({ left: 270, behavior: "smooth" });
+  });
+
+  prev.addEventListener("click", () => {
+    slider.scrollBy({ left: -270, behavior: "smooth" });
+  });
+});
